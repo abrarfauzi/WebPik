3b61867eaf598cd45386b6b020d5e1e53cda6026
abrar <abrar.fauzi@e.braude.ac.il>
2024-04-20 10:51:44 +0300

    first commit

--- backend/models/estateModel.js
+++ backend/models/estateModel.js
@@ -3,11 +3,7 @@ const Schema = mongoose.Schema;
 
 const estateSchema = new Schema(
   {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
+    name: {
       type: String,
       required: true,
     },
